0f4df138cf6aea22e1733f4ac11c217a00ecd363
Kuldeep Doneriya <dkuldeep@vmware.com>
2023-11-10 17:03:06 +0530

    adding CSP OAuth token

--- dist/module.d.ts
+++
@@ -1,8 +0,0 @@
-import { WavefrontDatasource } from "./plugin/datasource";
-import { WavefrontConfigCtrl } from "./plugin/configCtrl";
-import { WavefrontQueryCtrl } from "./plugin/queryCtrl";
-import { WavefrontQueryOptionsCtrl } from "./plugin/queryOptionsCtrl";
-declare class AnnotationsQueryCtrl {
-    static templateUrl: string;
-}
-export { WavefrontDatasource as Datasource, WavefrontConfigCtrl as ConfigCtrl, WavefrontQueryCtrl as QueryCtrl, WavefrontQueryOptionsCtrl as QueryOptionsCtrl, AnnotationsQueryCtrl as AnnotationsQueryCtrl, };
--- dist/plugin/configCtrl.d.ts
+++
@@ -1,9 +0,0 @@
-export declare class WavefrontConfigCtrl {
-    static templateUrl: string;
-    current: any;
-    wavefrontTokenExists: boolean;
-    cspApiTokenExists: boolean;
-    constructor();
-    resetWavefrontToken(): void;
-    resetCspApiToken(): void;
-}
